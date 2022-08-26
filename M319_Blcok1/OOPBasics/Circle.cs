745f129a6d3619a69b986ebe2c94ca189cd0e5c8
Grandinho <monyx5861@gmail.com>
2022-08-26 09:50:31 +0200

    Added first Block
    First block from Module320. Introducting us to Object Oriented Programming

---
+++ M319_Blcok1/OOPBasics/Circle.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPBasics
+{
+    class Circle
+    {
+        //Membervariablen
+        public double _diameter;
+
+
+        //Konstuktoren
+
+        //Methoden
+
+        public double CircleArea()
+        {
+            return Math.Pow(_diameter, 2) * Math.PI / 4;
+        }
+
+
+    }
+}
---
+++ M319_Blcok1/OOPBasics/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPBasics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Circle c1 = new Circle();
+
+            Circle c2 = new Circle();
+
+            c2._diameter = 1;
+            double k = c2.CircleArea();
+        }
+    }
+}
